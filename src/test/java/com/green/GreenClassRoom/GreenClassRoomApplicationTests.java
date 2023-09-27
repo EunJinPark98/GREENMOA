b93f14efee82286ab084e720977a9e37d6bf9a9c
EunJinPark98 <obliviscor29@naver.com>
2023-09-27 15:40:07 +0900

    first

---
+++ src/test/java/com/green/GreenClassRoom/GreenClassRoomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.green.GreenClassRoom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenClassRoomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
