42ec6f5c259ea209055ffdf02d00844e289cdaec
guqwls5442 <guqwls5442@gmail.com>
2023-10-04 16:16:43 +0900

    두번째 수정

--- src/main/java/com/green/GreenClassRoom/member/service/MemberServiceImpl.java
+++ src/main/java/com/green/GreenClassRoom/member/service/MemberServiceImpl.java
@@ -1,5 +1,6 @@
 package com.green.GreenClassRoom.member.service;
 
+import com.green.GreenClassRoom.member.vo.MemberVO;
 import lombok.RequiredArgsConstructor;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.stereotype.Service;
@@ -8,4 +9,15 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class MemberServiceImpl implements MemberService {
     private final SqlSessionTemplate sqlSession;
+
+    @Override
+    public int join(MemberVO memberVO) {
+        return sqlSession.insert("memberMapper.join", memberVO);
+    }
+
+    @Override
+    public boolean checkId(String memberId) {
+        String selectMemberId = sqlSession.selectOne("memberMapper.checkId", memberId);
+        return selectMemberId == null ? true : false;
+    }
 }
