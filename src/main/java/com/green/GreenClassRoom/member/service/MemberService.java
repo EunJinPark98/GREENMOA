e426f3f07bec3deb15cf6de5f5d3a70dd142f0b6
guqwls5442 <guqwls5442@gmail.com>
2023-10-05 17:24:57 +0900

    미니미 실패

--- src/main/java/com/green/GreenClassRoom/member/service/MemberService.java
+++ src/main/java/com/green/GreenClassRoom/member/service/MemberService.java
@@ -5,7 +5,7 @@ import com.green.GreenClassRoom.member.vo.MemberVO;
 public interface MemberService {
 
     //회원가입
-    public int join(MemberVO memberVO);
+//    public int join(MemberVO memberVO);
 
     //아이디 중복 확인
     public boolean checkId(String memberId);
