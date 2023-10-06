b0aa93b775a8d0b4eae1f7c806e69a16694c2769
guqwls5442 <guqwls5442@gmail.com>
2023-10-06 16:58:12 +0900

    질문게시판

---
+++ src/main/java/com/green/GreenClassRoom/board/service/QnaBoardServiceImpl.java
@@ -0,0 +1,24 @@
+package com.green.GreenClassRoom.board.service;
+
+import com.green.GreenClassRoom.board.vo.QnaBoardVO;
+import lombok.RequiredArgsConstructor;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class QnaBoardServiceImpl implements QnaBoardService{
+    private final SqlSessionTemplate sqlSession;
+
+    @Override
+    public void insert(QnaBoardVO qnaBoardVO) {
+        sqlSession.insert("qnaBoardMapper.insert", qnaBoardVO);
+    }
+
+    @Override
+    public List<QnaBoardVO> selectQnaBoard(QnaBoardVO qnaBoardVO) {
+        return sqlSession.selectList("qnaBoardMapper.selectQnaBoard", qnaBoardVO);
+    }
+}
