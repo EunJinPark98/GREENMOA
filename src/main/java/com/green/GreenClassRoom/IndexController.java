90cc95f51f581201870c1a9492f4e09fffcff6cd
gpffhdn963 <gpffhdn963@naver.com>
2023-10-10 17:42:01 +0900

    댓글 수정, 로그인 수정

--- src/main/java/com/green/GreenClassRoom/IndexController.java
+++ src/main/java/com/green/GreenClassRoom/IndexController.java
@@ -1,12 +1,23 @@
 package com.green.GreenClassRoom;
 
+import com.green.GreenClassRoom.member.service.MemberService;
+import com.green.GreenClassRoom.member.vo.MemberVO;
+import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@RequiredArgsConstructor
 public class IndexController {
+    final private MemberService memberService;
+
     @GetMapping("/")
-    public String index(){
+    public String index(MemberVO memberVO, HttpSession session){
+        MemberVO loginInfo=memberService.login(memberVO);
+        if(loginInfo!=null) {
+            session.setAttribute("loginInfo", loginInfo);
+        }
         return "redirect:/room/main";
     }
 }
