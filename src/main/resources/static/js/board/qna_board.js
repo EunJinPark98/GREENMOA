293f2a8af5b825102ff7a055c2befb753be699d3
guqwls5442 <guqwls5442@gmail.com>
2023-10-13 15:29:26 +0900

    질문 게시판 완료

--- src/main/resources/static/js/board/qna_board.js
+++ src/main/resources/static/js/board/qna_board.js
@@ -13,10 +13,18 @@ function updateQnaBoard(qnaBoardNum){
 
 // 댓글 삭제 버튼
 function deleteQnaReply(qnaBoardNum){
-    var qnaReplyNum = document.getElementById("qnaReplyNum");
-    qnaReplyNum = qnaReplyNum.dataset.qnaReplyNum;
-    const result = confirm('삭제하시겠습니까?');
-    if(result){
-        location.href=`/board/deleteQnaReply?qnaReplyNum=${qnaReplyNum}&qnaBoardNum=${qnaBoardNum}`;
+     // qnaReplyNum 요소를 선택
+     var qnaReplyNumElement = document.querySelector('#qnaReplyNum');
+    
+     //이거는 define이 뜨는데 왜? 
+     //alert(qnaReplyNumElement.dataset.dataReplyNum);
+
+     // data-reply-num 속성의 값을 가져옴
+     var dataReplyNumValue = qnaReplyNumElement.getAttribute('data-reply-num');
+
+    if(confirm('삭제하시겠습니까?')){
+        location.href=`/board/deleteQnaReply?qnaReplyNum=${dataReplyNumValue}&qnaBoardNum=${qnaBoardNum}`;
     };
+
 };
+
