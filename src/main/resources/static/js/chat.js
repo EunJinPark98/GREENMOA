a4c1350025624ca2c64e37bb43b48a816c592599
EunJinPark98 <obliviscor29@naver.com>
2023-10-13 16:56:55 +0900

    채팅 거의 완료

--- src/main/resources/static/js/chat.js
+++ src/main/resources/static/js/chat.js
@@ -1,63 +1,5 @@
-// // STOMP
-// var stompClient = null;
-
-
-// function connect() {
-//     //var socket = new SockJS('/chat');
-//     var socket = new SockJS('http://192.168.30.55:8081/chat');
-//     stompClient = Stomp.over(socket);
-//     stompClient.connect({}, function (frame) {
-//         console.log('연결상태: ' + frame);
-//         stompClient.subscribe('/topic', function (message) {
-//             showMessage(JSON.parse(message.body).content);
-//         });
-
-//     }); 
-// }
-
-// // 메시지 전송
-// function sendMessage() {
-//     var messageInput = document.getElementById('message');
-//     var message = messageInput.value;
-//     stompClient.send("/app/chat", {}, JSON.stringify({'content': message, 'sender': "익명" }));
-
-
-//     messageInput.value = '';
-// }
-
-// // 메시지 표시
-// function showMessage(message) {
-//     var chatMessages = document.getElementById('chat-messages');
-//     var msgEl = document.createElement('p');
-//     msgEl.appendChild(document.createTextNode(message));
-//     chatMessages.appendChild(msgEl);
-
-//     scrollToBottom();
-// }
-
-// // 페이지 로드 시 채팅 연결
-// document.addEventListener('DOMContentLoaded', function () {
-//     connect();
-    
-//     var messageForm = document.getElementById('message-form');
-//     messageForm.addEventListener('submit', function (e) {
-//         e.preventDefault();
-//         sendMessage();
-//     });
-// });
-
-
-
-
-
-
-
-
-//WebSocketConfigurer
-
-
-//var socket = new SockJS('http://192.168.30.55:8081/chat');
 var socket = new WebSocket('ws://192.168.30.55:8081/chat');
+
 
 socket.onopen = function() {
     console.log('연결이 열렸습니다.');
@@ -96,22 +38,24 @@ function showMessage(message) {
     chatMessages.appendChild(msgEl);
 }
 
-// 페이지 로드 시 WebSocket 연결
+
+
 window.onload = function() {
-    connect();
-    var messageForm = document.getElementById('message-form');
-    messageForm.addEventListener('submit', function (e) {
-        e.preventDefault();
-        sendMessage();
-    });
+    
         
 };
 
 
+var messageForm = document.getElementById('message-form');
+    messageForm.addEventListener('submit', function (e) {
+        e.preventDefault();
+        sendMessage();
+
+        scrollToBottom()
+    });
 
 
-
-
+    
 
 // 채팅 스크롤바 항상 밑에 있도록
 function scrollToBottom() {
