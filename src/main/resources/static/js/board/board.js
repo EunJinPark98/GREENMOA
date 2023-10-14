1f134ee0726cab4f7b578fd2cfa45f3553c07cdb
yoooojl <134619185+yoooojl@users.noreply.github.com>
2023-10-15 06:58:23 +0900

    깨작

--- src/main/resources/static/js/board/board.js
+++ src/main/resources/static/js/board/board.js
@@ -19,23 +19,3 @@ function deleteReply(boardNum){
         location.href=`/board/deleteReply?replyNum=${replyNum}&boardNum=${boardNum}`;
     };
 };
-
-
-
-// 엔터키 누르면 자동 댓글 등록버튼 동작실행
-// 시프트+엔터해야 줄바꿈되도록
-let replyText = document.getElementById('replyText');
-replyText.addEventListener('keydown', function(event){
-    if(event.key === 'Enter' && !event.shiftKey){
-        event.preventDefault();
-        submitToForm();
-    }
-
-});
-function submitToForm(){
-    const replyForm = document.getElementById('replyForm');
-    replyForm.addEventListener('submit', function(event){
-        event.preventDefault();
-    })
-    replyForm.submit();
-};
