a2e83d84f4980703e0203ce26e26fce15a15f919
guqwls5442 <guqwls5442@gmail.com>
2023-10-23 12:28:29 +0900

    summernote

--- src/main/resources/static/js/board/qna_board_insert_page.js
+++ src/main/resources/static/js/board/qna_board_insert_page.js
@@ -6,17 +6,23 @@ $(document).ready(function() {
         maxHeight : null,
         focus : true,
         lang : 'ko-KR',
-        // fontName : Dovemayo_gothic,
-        // fontNameIgnoreCheck : Dovemayo_gothic,
-        // toolbar : [
-        //     // [groupname, [list of button]]
-        //     ['fontsize', ['fontsize']],
-        //     ['style', ['bold', 'italic', 'underline', 'strikethrough', 'clear']],
-        //     ['color', ['forecolor', 'color']],
-        //     ['table', ['table']],
-        //     ['para', ['ul', 'oi', 'paragraph']],
-        //     ['height', ['height']],
-        // ],
-        // fontSize : ['8', '9', '10', '11', '12', '14', '16', '18', '20', '22', '24', '28', '30', '36', '50', '72']
-    });
+        toolbar: [
+            // [groupName, [list of button]]
+            ['fontname', ['fontname']],
+            ['fontsize', ['fontsize']],
+            ['style', ['bold', 'italic', 'underline','strikethrough', 'clear']],
+            ['color', ['forecolor','color']],
+            ['table', ['table']],
+            ['para', ['ul', 'ol', 'paragraph']],
+            ['height', ['height']],
+           // ['insert',['picture','link','video']],
+            ['view', ['fullscreen', 'help']]
+        ],
+        fontNames: ['Dovemayo_gothic', 'Arial', 'Arial Black', 'Comic Sans MS', 'Courier New','맑은 고딕','궁서','굴림체','굴림','돋움체','바탕체'],
+        fontNameIgnoreCheck : ['Dovemayo_gothic', 'Arial', 'Arial Black', 'Comic Sans MS', 'Courier New','맑은 고딕','궁서','굴림체','굴림','돋움체','바탕체'],
+        fontSizes: ['8','9','10','11','12','14','16','18','20','22','24','28','30','36','50','72']
+    })
   });
+
+
+
