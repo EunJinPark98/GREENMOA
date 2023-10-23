930d14a0439c0d854edd32664d869ccf8637e9bf
EunJinPark98 <obliviscor29@naver.com>
2023-10-23 17:08:57 +0900

    모달클릭이 안됨

---
+++ src/main/java/com/green/GreenClassRoom/room/vo/CalenderVO.java
@@ -0,0 +1,10 @@
+package com.green.GreenClassRoom.room.vo;
+
+import lombok.Data;
+
+@Data
+public class CalenderVO {
+    private int calNum;
+    private String calTitle;
+    private String calDate;
+}
