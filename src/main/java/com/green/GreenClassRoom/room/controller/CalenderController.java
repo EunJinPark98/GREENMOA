efd0ddcbce64884d3b9f922d51e33df5269eee02
EunJinPark98 <obliviscor29@naver.com>
2023-10-24 15:07:29 +0900

    일정추가

--- src/main/java/com/green/GreenClassRoom/room/controller/CalenderController.java
+++ src/main/java/com/green/GreenClassRoom/room/controller/CalenderController.java
@@ -1,10 +1,22 @@
 package com.green.GreenClassRoom.room.controller;
 
+import com.green.GreenClassRoom.room.service.CalenderService;
+import com.green.GreenClassRoom.room.vo.CalenderVO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class CalenderController {
 
+    private final CalenderService calenderService;
 
-
+    @PostMapping("/addEvent")
+    public ResponseEntity<String> addEvent(@RequestBody CalenderVO calenderVO) {
+        calenderService.insertCalender(calenderVO);
+        return ResponseEntity.ok("Event added successfully");
+    }
 }
--- src/main/java/com/green/GreenClassRoom/room/service/CalenderService.java
+++ src/main/java/com/green/GreenClassRoom/room/service/CalenderService.java
@@ -1,7 +1,14 @@
 package com.green.GreenClassRoom.room.service;
 
+import com.green.GreenClassRoom.room.vo.CalenderVO;
+
+import java.util.List;
+
 public interface CalenderService {
 
     //캘린더 조회
+    public List<CalenderVO> selectCalender();
 
+    //캘린더 일정 추가
+    public void insertCalender(CalenderVO calenderVO);
 }
